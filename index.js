a1833af26355d37f2cf777fffe4491201169aa72
IcaroAguiar <icaroaguiar14@gmail.com>
2022-06-17 18:44:36 -0300

    Primeira versão do StopWatch

---
+++ index.js
@@ -0,0 +1,59 @@
+let tempo = 1000;
+let cronometro;
+let hh = 0;
+let min = 0;
+let sec = 0;
+let button = 0;
+
+
+function start(){
+    button++;
+    if(button<2){
+    cronometro =  setInterval(() => { timer();}, tempo);
+    }
+
+
+}
+
+function stop(){
+    button = 0;
+    clearInterval(cronometro);
+    hh = hh;
+    min = min;
+    sec = sec;
+
+}
+
+function reset(){
+    button=0;
+    clearInterval(cronometro);
+
+    hh = 0;
+    min = 0;
+    sec = 0;
+
+    document.getElementById('hora').innerText = hh
+    document.getElementById('minutos').innerText = min
+    document.getElementById('segundos').innerText = sec
+}
+
+function timer(){
+    sec++;
+
+    if(sec == 60){
+        sec = 0;
+        min++;
+
+        if(min==60){
+            min =0;
+            hr++;
+        }
+
+    }
+    document.getElementById('hora').innerText = hh
+    document.getElementById('minutos').innerText = min
+    document.getElementById('segundos').innerText = sec
+
+
+
+}
